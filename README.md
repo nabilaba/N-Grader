315517c2341161cf7d8cd5839fc21a9d305a530a
nabilaba <nabilazizbimaanggita@gmail.com>
2022-04-11 07:45:02 +0700

    Update to version 2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # N-Grader
+## Changelog
+### Version 2
+- Added support ansicon
+- More control
+- Update submit
+
+### Version 1
+- Initial Release
--- n-grader.bat
+++ n-grader.bat
@@ -1,39 +1,76 @@
-:: n-grader
+:: n-grader v2
 :: build by nabil_aba
-:: hasil natap leptop satu hari satu malam
 :: my-github: github.com/nabilaba
 :: copyright 2022
 
-@echo off
+@echo off & setlocal
 cls
+if "%ProgramFiles(x86)%" == "C:\Program Files (x86)" goto 64bit
+:32Bit
+"%~dp0ansicon\x86\ansicon.exe" -p
+goto cek64bitsupportdone
+
+:64bit
+"%~dp0ansicon\x64\ansicon.exe" -p
+:cek64bitsupportdone
+
 echo %cd%
+echo.
+echo [92mn-grader v2
+echo build by nabil_aba[0m
+echo.
 for /f %%i in ('grader-cli version') do set versi-grader-cli=%%i
-echo ********************************
-echo *         n-grader v1          *
-echo *      build by nabil_aba      *
-echo ********************************
-echo.
 for %%* in (.) do set n-grader-namafolder=%%~n*
-echo List sesuai grader-cli versi %versi-grader-cli%
-echo 1. Test "%n-grader-namafolder%" saja
-echo 2. Test "%n-grader-namafolder%" pakai benar salah
-echo 3. Submit "%n-grader-namafolder%" ke Grader Ruangguru
+echo List sesuai grader-cli versi [93m%versi-grader-cli%[0m
+echo 1. Test [96m%n-grader-namafolder%[0m saja
+echo 2. Test [96m%n-grader-namafolder%[0m pakai benar salah
+echo 3. Submit [96m%n-grader-namafolder%[0m ke Grader Ruangguru
+echo. & goto milih
+
+:test1
+echo Okey, test [96m%n-grader-namafolder%[0m sedang berjalan
 echo.
-set /p pilihan= "Di folder "%n-grader-namafolder%" mau ngapain? "
-IF %pilihan%==1 (
-echo Okey, test %n-grader-namafolder% sedang berjalan
 grader-cli test ../%n-grader-namafolder%
 echo.
-)
-if %pilihan%==2 (
-echo Okey, test %n-grader-namafolder% pakai benar salah sedang berjalan
+echo Sudah selesai
+goto end
+
+:test2
+echo Okey, test [96m%n-grader-namafolder%[0m pakai benar salah sedang berjalan
+echo.
 grader-cli test ../%n-grader-namafolder% -v
 echo.
-)
-if %pilihan%==3 (
-echo Okey, submit %n-grader-namafolder% ke grader ruangguru sedang berjalan
-grader-cli test ../%n-grader-namafolder%
+echo Sudah selesai
+goto end
+
+:yakinsubmit
+set /p yakinga= "Yakin mau submit? (Y/N) "
+if %yakinga%==Y goto submit
 echo.
-)
-echo Dah ya, Tekan Enter untuk keluar
+echo Yaudah gajadi
+goto end
+
+:submit
+echo Okey, submit [96m%n-grader-namafolder%[0m ke grader ruangguru sedang berjalan
+echo.
+grader-cli submit ../%n-grader-namafolder%
+echo.
+echo Sudah selesai
+goto end
+
+:milih
+set /p pilihan= "Di folder [96m%n-grader-namafolder%[0m mau ngapain? "
+if %pilihan%==1 goto test1
+if %pilihan%==2 goto test2
+if %pilihan%==3 goto yakinsubmit
+
+:gamilih
+echo.
+echo Pilihanmu ga ada di list
+echo Pilih pakek nomer saja
+echo.
+
+:end
+echo.
+echo Tekan Enter untuk menutup
 pause >nul
